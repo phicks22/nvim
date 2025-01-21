919e0fd5d1d89e761a196d1330cff1ba0ba97ab9
phicks22 <65268541+phicks22@users.noreply.github.com>
2025-01-20 19:01:12 -0700

    upload plugins

--- lua/phicks/core/options.lua
+++ lua/phicks/core/options.lua
@@ -7,8 +7,8 @@ opt.relativenumber = true
 opt.number = true
 
 -- Tabs and indentations
-opt.tabstop = 4         -- Spaces for tabs
-opt.shiftwidth = 4      -- Spaces for indent width
+opt.tabstop = 2         -- Spaces for tabs
+opt.shiftwidth = 2      -- Spaces for indent width
 opt.expandtab = true    -- Expand tab to spaces
 opt.autoindent = true   -- Copy indent from current line
 
---
+++ lua/phicks/plugins/substitute.lua
@@ -0,0 +1,19 @@
+-- When cursor over word, use ys, iw, then " to surround with quotes
+
+return {
+	"gbprod/substitute.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local substitute = require("substitute")
+
+		substitute.setup()
+
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
+		keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
+		keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
+		keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+	end,
+}
